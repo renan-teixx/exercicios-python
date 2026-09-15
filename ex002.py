9fedbbd65f19b768b2c96372e6e8b7587834a755
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-14 22:47:46 -0300

    feat: adiciona exercicio de soma de dois numeros inteiros

--- ex002.py
+++ ex002.py
@@ -1,8 +1,7 @@
-nome = input('Digite o seu nome: ')
+nome = input("Digite o seu nome: ")
 
 # Essa é uma das formas de fazer, usando o '{}' e '.format'
-print('É um prazer te conhecer {}! (saída com .format)' .format(nome))
+print('É um prazer te conhecer {}! (saída com .format)'.format(nome))  # noqa: UP032
 
 # Também é possível fazer de uma maneira mais simples, usando 'f' antes do texto e colocando as chaves para adicionar a variável 'nome':
 print(f'É um prazer te conhecer {nome}! (saída com f e chaves)')
-    
---
+++ ex003.py
@@ -0,0 +1,8 @@
+print('Esse programa realiza a soma de dois números inteiros digitados.')
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+resultado = valor1 + valor2
+
+print(f'O resultado da soma dos dois valores digitados é: {resultado}')
