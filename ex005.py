eb400e699eb32e1e2f7c2401c3ae1164ceb2ecb7
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-15 12:52:51 -0300

    feat: adiciona exercicio de antecessor e sucessor

---
+++ ex005.py
@@ -0,0 +1,8 @@
+print('Este programa apresenta o antecessor e o sucessor de um número inteiro digitado.')
+
+numero = int(input('Digite um número inteiro: '))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'O antecessor é: {antecessor}\nO sucessor é: {sucessor}')
