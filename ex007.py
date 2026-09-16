349a816d0016491eb7f5c66b4fb7557b05d1061a
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-16 16:26:09 -0300

    feat: adiciona exercicio de media aritmetica de duas notas

---
+++ ex007.py
@@ -0,0 +1,8 @@
+print('Programa que calcula a média aritmética de duas notas do aluno.')
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A média do aluno é: {media:.2f}')
