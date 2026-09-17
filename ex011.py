1b96dece512cc6f462b32f50e7004cb1595b6d0b
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-17 20:35:13 -0300

    feat: adiciona exercicio de calculo da area e de quantidade de litros de tinta de uma parede

---
+++ ex011.py
@@ -0,0 +1,9 @@
+print('Programa que calcula área de uma parede e quantidade de litros de tinta necessários para pintá-la.')
+
+altura = float(input('Digite a altura da parede: '))
+largura = float(input('Digite a largura da parede: '))
+
+area = altura * largura
+litrosTinta = area / 2
+
+print(f'A área da parede é de {area:.2f}m²\nSerão necessários {litrosTinta:.2f} litros de tinta para pintá-la')
