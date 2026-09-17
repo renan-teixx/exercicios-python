436850a9bacbbea8d7dbf900bed19384f583dcf5
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-17 19:56:01 -0300

    feat: adiciona exercicio de tabuada sem usar loopings

---
+++ ex009.py
@@ -0,0 +1,17 @@
+print('Programa que mostra a tabuada de um número inteiro digitado (sem usar loopings).')
+
+numero = int(input('Digite um número inteiro: '))
+
+print('-' * 12)
+print(f'{numero} x  1 = {numero*1}',
+        f'\n{numero} x  2 = {numero*2}',
+        f'\n{numero} x  3 = {numero*3}',
+        f'\n{numero} x  4 = {numero*4}',
+        f'\n{numero} x  5 = {numero*5}',
+        f'\n{numero} x  6 = {numero*6}',
+        f'\n{numero} x  7 = {numero*7}',
+        f'\n{numero} x  8 = {numero*8}',
+        f'\n{numero} x  9 = {numero*9}',
+        f'\n{numero} x 10 = {numero*10}',
+    )
+print('-' * 12)
