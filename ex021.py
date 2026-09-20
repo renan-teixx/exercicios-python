cf174886b30734dee86457963911127123540637
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-20 20:42:04 -0300

    feat: adiciona exercicio de inversao de ordem de numeros com vetor

---
+++ ex021.py
@@ -0,0 +1,10 @@
+print('Programa que mostra os números digitados na ordem inversa.')
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+n4 = int(input('Digite o quarto número: '))
+
+numeros = [n4, n3, n2, n1]
+
+print(f'A ordem inversa dos números digitados é {numeros}')
