85c522d16a0ef616d9b61f4bdcac3175cb43146a
Renan de Castro Teixeira <145163030+RenanCT2004@users.noreply.github.com>
2026-09-22 13:52:12 -0300

    feat: adiciona exercicio de par ou impar

---
+++ ex030.py
@@ -0,0 +1,9 @@
+print('Programa que verifica se o número é par ou ímpar.')
+
+numero = int(input('Digite um número inteiro: '))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é par!')
+else:
+    print(f'O número {numero} é ímpar!')
+    
